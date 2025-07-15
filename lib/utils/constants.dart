51e4a80602569db5c93a94cf78815810c448809e
Toprak <topraktas19@hotmail.com>
2025-07-16 00:10:18 +0300

    properties for contact information added

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+abstract final class ContactProperties {
+  static const String name = 'Name';
+  static const String mobile = 'Mobile';
+  static const String email = 'Email';
+  static const String profession = 'Profession';
+  static const String address = 'Address';
+  static const String company = 'Company';
+  static const String website = 'Website';
+}
