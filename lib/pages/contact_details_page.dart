71202b609de43a671badb5f2b71552df99b0415d
Toprak <topraktas19@hotmail.com>
2025-08-23 21:25:22 +0300

    Contact details added

--- lib/pages/contact_details_page.dart
+++ lib/pages/contact_details_page.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:vcard/models/contact_model.dart';
 import 'package:vcard/providers/contact_provider.dart';
+import 'package:vcard/utils/helper_functions.dart';
 
 class ContactDetailsPage extends StatefulWidget {
   static const String routeName = 'details';
@@ -44,6 +47,53 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
                         height: 250,
                         fit: BoxFit.cover,
                       ),
+                      ListTile(
+                        title: Text(contact.mobile),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                callContact(contact.mobile);
+                              },
+                              icon: Icon(Icons.call),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                smsContact(contact.mobile);
+                              },
+                              icon: Icon(Icons.sms),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(contact.email),
+                        trailing: IconButton(
+                          onPressed: () {
+                            mailContact(contact.email);
+                          },
+                          icon: Icon(Icons.email),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(contact.website),
+                        trailing: IconButton(
+                          onPressed: () {
+                            websiteContact(contact.website);
+                          },
+                          icon: Icon(Icons.language),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(contact.address),
+                        trailing: IconButton(
+                          onPressed: () {
+                            addressContact(contact.address);
+                          },
+                          icon: Icon(Icons.location_on),
+                        ),
+                      ),
                     ],
                   );
                 }
@@ -55,5 +105,38 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
             ),
       ),
     );
+  }
+
+  void callContact(String mobile) async {
+    final url = 'tel:$mobile';
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      showMessage(context, 'Cannot call this phone number!');
+    }
+  }
+
+  void smsContact(String mobile) async {
+    final url = 'sms:$mobile';
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else {
+      showMessage(context, 'Cannot send sms to this phone number!');
+    }
+  }
+
+  void mailContact(String email) async {
+    final url = Uri(scheme: 'mailto', path: email);
+    await launchUrl(url);
+  }
+
+  void websiteContact(String website) async {
+    final url = Uri(scheme: 'https', path: website);
+    await launchUrl(url);
+  }
+
+  void addressContact(String address) async {
+    final url = 'geo:0,0?q=$address';
+    await launchUrlString(url);
   }
 }
